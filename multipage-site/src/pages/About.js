fc10da03954c4255015226eb2e6683abe445d7a2
a1623784 <a1623784@bestbuy.com>
2022-02-16 16:10:50 -0600

    Add Router

---
+++ multipage-site/src/pages/About.js
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Exercitation nulla eu cupidatat qui Lorem esse tempor do labore id irure irure non ex. Eu
+                culpa ad dolor labore veniam ut minim elit eiusmod. Consectetur cillum do cupidatat do veniam ut est laboris non aute amet sit.
+                Tempor ex reprehenderit dolore culpa magna mollit sit elit occaecat mollit consequat. Consequat fugiat minim pariatur duis aliquip dolor fugiat ipsum nulla eiusmod ex voluptate. Officia dolore consectetur nisi nisi
+                magna cupidatat occaecat eu exercitation dolor voluptate.
+            </p>
+        </div>
+    )
+}
